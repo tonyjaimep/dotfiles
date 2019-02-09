5c4d34868138cd1333356396934c1f4de79e1d80
tonyjaimep <tonyjaimep@gmail.com>
2019-02-08 22:22:09 -0600

    I started using emacs

--- fish/config.fish
+++ fish/config.fish
@@ -1,37 +1,47 @@
+# navigation
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
+# apt
 alias install="sudo apt install"
 alias update="sudo apt update"
 alias uninstall="sudo apt remove"
 
-alias eventour="cd ~/Code/Web/Eventour/; clear"
-alias antonio="cd ~/Code/Web/antonio/; clear"
-alias union="cd ~/Code/Web/Union/; clear"
-alias boletos="cd ~/Code/Web/Boletos/; clear"
-alias trivix="cd ~/Code/Web/Trivix/; clear"
-
-alias graph="exec wine /home/anon/.wine/drive_c/Program\ Files\ \(x86\)/Graph/Graph.exe"
-alias arduino="~/Downloads/arduino-1.8.5/arduino"
-
+# most used programs
 alias v="vim"
+alias e="emacs -nw"
+alias g="git"
+alias py3="python3.6m"
+alias g++17="g++ -std=gnu++17"
 
+# courtesy is key
 alias thanks="echo You\'re welcome"
 
+# play a youtube video in given resolution
 function youtube
 	mpv "https://www.youtube.com/watch?v=$argv[1]" --ytdl-format=bestvideo[height<=$argv[2]]+bestaudio
 end
 
-alias g++17="g++ -std=gnu++17"
-
-set fish_color_cwd cyan
-set fish_color_quote green
-set fish_color_param blue
-
-function fish_greeting
-	fortune -as
+# start new laravel project
+function new-laravel-project
+	cd ~/Code/Web/;
+	laravel new $argv[1];
+	cd $argv[1];
+	npm install;
 end
 
+# fish defaults
+set fish_color_cwd brblack
+set fish_color_quote green
+set fish_color_param blue
+set fish_vi_keybindings
+
+# display a random phrase from "The art of war" when terminal is started.
+function fish_greeting
+	awk "/^[0-9]/" ~/Books/artofwar.txt | shuf -n 1
+end
+
+# add composer to path
 set -gx PATH ~/.composer/vendor/bin/ $PATH
